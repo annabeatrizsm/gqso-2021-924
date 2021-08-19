0a55d9f981e7f14cee020f19d76f7b62dc0163e9
annabeatrizsm <annabeatrizsm47@gmail.com>
2021-08-19 01:57:36 +0000

    alterando cod soma

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -1,21 +1,19 @@
 package app;
 import io.jooby.annotations.*;
 import io.jooby.exception.*;
-import io.jooby.exception.BadRequestException.*;
-
 
 public class Soma{
-    @Path("/soma/{op}/{ap}")
-    @GET
-    public String Soma (@PathParam String op, @PathParam String ap){
-        // Converter op e ap para double (verificar erros)
-        try{
-            double sqrt = Math.sqrt (Double.parseDouble(op) + Double parseDouble(ap));
-        // Retornar soma op+ap 
-            return String.format("%s.2d",sqrt);
-        }catch (NumberFormaException nfe){
-            throw new BadRequestException(String.format("Inválido:\"%s\"",op, ap));
+    @Path ( "/soma/{b}/{m}")
+    @GET 
+    public String rotaDaSoma(@PathParam String b, @PathParam String m){
+        try {
+            double s = Double.parseDouble(b) + Double.parseDouble(m);
+
+            return String.format("%.1f", s);
+        } 
+        catch (NumberFormatException nfe) {
+           throw new BadRequestException(String.format(" Parâmetro incorreto "));
         }
-       
+        
     }
 }
